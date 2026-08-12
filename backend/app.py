4dc0eaf2a031f2e8df5715861485cae757656e4d
sandhyashrestha73 <sandhyashrestha431@gmail.com>
2026-08-12 20:51:19 +0545

    Database Connection

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,18 @@ from flask_cors import CORS
 from config import Config
 from database import db
 
+
+from models.tourists import Tourist
+from models.agency import Agency
+from models.guide import Guide
+from models.destination import Destination
+from models.package import Package
+from models.booking import Booking
+from models.blog import Blog
+from models.gallery import Gallery
+from models.admin import Admin
+
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
@@ -17,6 +29,23 @@ db.init_app(app)
 def home():
     return "Welcome to TourEase Nepal"
     
+
+@app.route("/api/test-db")
+def test_db():
+    try:
+        db.session.execute(db.text("SELECT 1"))
+        return {
+                "success": True,
+                "message": "Database connection successful!"
+            }  
+        
+    except Exception as e:
+        return {
+             "success": False,
+            "error": str(e)
+        }, 500
+    
+
 
 
 if __name__ == "__main__":
--- backend/config.py
+++ backend/config.py
@@ -1,15 +1,17 @@
 import os
+from urllib.parse import quote_plus
 from dotenv import load_dotenv
 
 load_dotenv()
 
+DB_USER = os.getenv("DB_USER", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "")
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}/"
-        f"{os.getenv('DB_NAME')}"
+        f"mysql+pymysql://{DB_USER}:{quote_plus(DB_PASSWORD)}@{DB_HOST}/{DB_NAME}"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
